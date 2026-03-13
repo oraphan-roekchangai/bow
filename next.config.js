e569fc2071edcb70a1d5b2d0364fc7ec7230c577
imnotFlixZ <monster.om.sv2@gmail.com>
2026-03-13 22:08:06 +0700

    fix: update basePath and add rewrites for development environment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/admin",
+  basePath: '/admin',
+  async rewrites() {
+    return process.env.NODE_ENV === 'development' ? [
+      {
+        source: '/api/:path*',
+        destination: 'https://parking.vpscloud.cc/api/:path*',
+        basePath: false,
+      },
+    ] : [];
+  },
 };
+
 module.exports = nextConfig;
